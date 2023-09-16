5faca36c48ce3baf91356204f3f321a69f724839
Kulpot <kulpotwahu@gmail.com>
2023-09-16 21:47:29 +0800

    Add project files.

---
+++ Entity Framework Relationships Code First/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+
+//ref link:https://www.youtube.com/watch?v=WjbWwn3dHLk&list=PLRwVmtr-pp06bXl6mbwDfK1eW9sAIvWUZ&index=6
+// EntityFramework - is a object relational mapping(ORM) data tool
+// SQL Server Management Studio - Database app
+// SQL Server Profiler - app for tracing SQL Database
+
+// CRUD - Create, Read, Update, Delete
+
+
+/*  AppConfig
+ *  
+ *  <?xml version="1.0" encoding="utf-8" ?>
+<configuration>
+	<configSections>
+		<section name="entityFramework" type="System.Data.Entity.Internal.ConfigFile.EntityFrameworkSection, EntityFramework, Version=6.4.4, Culture=neutral"/>
+	</configSections>
+	<entityFramework>
+		<defaultConnectionFactory type="System.Data.Entity.Infrastructure.SqlConnectionFactory, EntityFramework"/>
+		<providers>
+			<provider invariantName="System.Data.SqlClient" type="System.Data.Entity.SqlServer.SqlProviderServices, EntityFramework.SqlServer"/>
+		</providers>
+	</entityFramework>
+<connectionStrings>
+	<add name="MeContext" connectionString="Data Source=.;Initial Catalog=MyTestDb;Integrated Security=True" providerName="System.Data.SqlClient"/>
+</connectionStrings>
+</configuration>
+ * 
+ * 
+ */
+
+
+
+class PlayList
+{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public List<Video> Videos { get; set; }
+}
+
+class Video
+{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
+
+class MeContext : DbContext
+{
+    //public MeContext() : base(@"Data Source=.;Initial Catalog=MyTestDb;username = whateverUsername;password = yadayadayada")
+    public MeContext() : base(@"Data Source=.;Initial Catalog=MyTestDb;Integrated Security=True")
+    {
+    }
+    public DbSet<Video> Videos { get; set; }
+}
+
+class MainClass
+{
+    static void Main()
+    {
+        MeContext db = new MeContext();
+        db.Database.Delete();
+        Video meVideo = new Video
+        {
+            Title = "Hello World Entity Framework",
+            Description = "Learn the entity framework"
+        };
+        PlayList mePlaylist = new PlayList();
+        mePlaylist.Title = "Entity Framework";
+        myPlaylist.Videos.Add(meVideo);
+
+
+
+        //// CRUD - Create, Read, Update, Delete
+        //var meContext = new MeContext();
+        //Video vid = meContext.Videos.FirstOrDefault();
+        //
+        //meContext.Videos.Remove(vid);   // --- Delete ---
+        //meContext.SaveChanges();
+
+        //vid.Title = "Beggining CRUD"; // --- Update ---
+        //meContext.SaveChanges();
+
+
+        //Console.WriteLine(vid.Title);   // --- Read ----
+        //Console.WriteLine(vid.Description); // --- Read ----
+
+        //Video vid = new Video     // ---- Create ----
+        //{
+        //    Title = "Entity Framework CRUD Operations",
+        //    Description = "Learn the meaning of CRUD."
+        //};
+        //meContext.Videos.Add(vid);
+        //meContext.SaveChanges();
+
+
+
+
+        //var vid = new Video
+        //{
+        //    Title = "Hello World Entity Framework",
+        //    Description = "Learn about the entity framework"
+        //};
+        //var meContext = new MeContext();
+
+
+        //Video meVideo = meContext.Videos.Single();    // ----Read----
+        //Console.WriteLine(meVideo.ID);
+        //Console.WriteLine(meVideo.Title);
+        //Console.WriteLine(meVideo.Description);
+
+
+
+        //meContext.Database.Delete();  // ----Delete---
+
+        //meContext.Videos.Add(vid);    // ----Create---
+        //meContext.SaveChanges();
+    }
+}
